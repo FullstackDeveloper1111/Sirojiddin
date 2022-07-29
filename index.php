2f0b44fa0ba9e515e92e5e808a4dd85672ef32af
sirojiddin02 <sirojiddin38.48@gmail.com>
2022-07-29 16:00:32 +0500

    yangilandi

--- index.php
+++ index.php
@@ -140,32 +140,7 @@
 
           </div>
 
-          <div class="col-lg-8 mt-5 mt-lg-0">
-
-            <form action="forms/gmail.php" method="post" role="form" class="php-email-form" autocomplete="off">
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-                </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-                </div>
-              </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-              </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" id="message" rows="6" placeholder="Message" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit" name="submit" id="name">Send Message</button></div>
-            </form>
-
-          </div>
+          
 
         </div>
 
